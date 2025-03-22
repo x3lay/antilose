e4ac8c7d2478929db3a79aa4fcb162622f664ae0
x3lay <x3layka@gmail.com>
2025-03-22 06:36:11 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,21 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = Home;
-var navbar_1 = require("@/components/navbar");
-var hero_section_1 = require("@/components/hero-section");
-var features_section_1 = require("@/components/features-section");
-var security_section_1 = require("@/components/security-section");
-var testimonials_section_1 = require("@/components/testimonials-section");
-var footer_1 = require("@/components/footer");
-function Home() {
-    return (<main className="animated-gradient-bg min-h-screen">
-      <navbar_1.default />
-      <hero_section_1.default />
-      <features_section_1.default />
-      <security_section_1.default />
-      <testimonials_section_1.default />
-      <footer_1.default />
-    </main>);
+"use client";
+
+import Navbar from "@/components/navbar";
+import HeroSection from "@/components/hero-section";
+import FeaturesSection from "@/components/features-section";
+import SecuritySection from "@/components/security-section";
+import TestimonialsSection from "@/components/testimonials-section";
+import Footer from "@/components/footer";
+
+export default function Home() {
+  return (
+    <main className="animated-gradient-bg min-h-screen">
+      <Navbar />
+      <HeroSection />
+      <FeaturesSection />
+      <SecuritySection />
+      <TestimonialsSection />
+      <Footer />
+    </main>
+  );
 }
